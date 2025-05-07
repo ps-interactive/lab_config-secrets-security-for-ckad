e3b527aeebf1f452671b4976bf58ef7891970a71
Shimon Brathwaite <46653837+Shimon242@users.noreply.github.com>
2025-05-07 10:50:20 -0400

    Add files via upload

---
+++ CP_setup.sh
@@ -0,0 +1,17 @@
+#Create our kubernetes cluster, specify a pod network range matching that in calico.yaml! 
+#Only on the Control Plane Node, download the yaml files for the pod network.
+wget https://raw.githubusercontent.com/projectcalico/calico/master/manifests/calico.yaml
+
+
+#You can now just use kubeadm init to bootstrap the cluster
+sudo kubeadm init --kubernetes-version v1.29.1
+
+#Configure our account on the Control Plane Node to have admin access to the API server from a non-privileged account.
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+
+#1 - Creating a Pod Network
+#Deploy yaml file for your pod network.
+kubectl apply -f calico.yaml
---
+++ Node.sh
@@ -0,0 +1,64 @@
+#Disable swap, swapoff then edit your fstab removing any entry for swap partitions
+#You can recover the space with fdisk. You may want to reboot to ensure your config is ok. 
+swapoff -a
+
+sudo apt-get update
+#Install a container runtime - containerd
+#containerd prerequisites, and load two modules and configure them to load on boot
+#https://kubernetes.io/docs/setup/production-environment/container-runtimes/
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+# sysctl params required by setup, params persist across reboots
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+# Apply sysctl params without reboot
+sudo sysctl --system
+
+#Install containerd...
+sudo apt-get install -y containerd
+
+#Configure containerd
+sudo mkdir -p /etc/containerd
+sudo containerd config default | sudo tee /etc/containerd/config.toml
+
+#You can use sed to swap in true
+sudo sed -i 's/            SystemdCgroup = false/            SystemdCgroup = true/' /etc/containerd/config.toml
+
+
+#Verify the change was made
+grep 'SystemdCgroup = true' /etc/containerd/config.toml
+
+
+#Restart containerd with the new configuration
+sudo systemctl restart containerd
+
+#Install Kubernetes packages - kubeadm, kubelet and kubectl
+#Add Google's apt repository gpg key
+sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+sudo curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+
+#Add the Kubernetes apt repository
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+
+#Update the package list and use apt-cache policy to inspect versions available in the repository
+sudo apt-get update
+apt-cache policy kubelet | head -n 20 
+
+#Install the required packages, if needed we can request a specific version. 
+#Use this version because in a later course we will upgrade the cluster to a newer version.
+#Try to pick one version back because later in this series, we'll run an upgrade
+VERSION=1.29.1-1.1
+sudo apt-get install -y kubelet=$VERSION kubeadm=$VERSION kubectl=$VERSION 
+sudo apt-mark hold kubelet kubeadm kubectl containerd
